f260611dbf9607b364b74a956cee478f620a14bf
Philipp <skrall_pa@protonmail.com>
2024-11-20 21:19:28 +0100

    add missing functionality

--- embeddings.py
+++ embeddings.py
@@ -1,10 +1,10 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Embedding, Flatten, Input, Dense
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 def to_number(labels):
@@ -36,3 +36,25 @@ def plot_vectors(model, tokenizer, indices=[0, 1, 2, 3, 4]):
 df = pd.read_csv("spam.csv", encoding="latin-1")
 train_texts = df.v2.tolist()
 train_labels = np.array(to_number(df.v1))
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+tokenized_texts = tokenizer.texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(tokenized_text) for tokenized_text in tokenized_texts)
+
+tokenized_texts = pad_sequences(tokenized_texts, maxlen=MAX_LENGTH, padding="post")
+
+get_vocab_indices(tokenizer)
+model = Sequential()
+model.add(Input(shape=(MAX_LENGTH,)))
+model.add(Embedding(vocab_size, 2, input_length=MAX_LENGTH))
+model.add(Flatten())
+model.add(Dense(10, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="sgd")
+model.fit(tokenized_texts, train_labels, epochs=5, batch_size=32, validation_split=0.2)
+
+check_words = [1337, 42, 1924, 8914, 5607]
+plot_vectors(model, tokenizer, check_words)
