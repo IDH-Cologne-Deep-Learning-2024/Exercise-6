d04bf586e6261b69227988650748fbbd203c8a6d
Jago- <Jago-@192.168.2.163>
2024-11-20 16:44:51 +0100

    done

--- embeddings.py
+++ embeddings.py
@@ -36,3 +36,41 @@ def plot_vectors(model, tokenizer, indices=[0, 1, 2, 3, 4]):
 df = pd.read_csv("spam.csv", encoding="latin-1")
 train_texts = df.v2.tolist()
 train_labels = np.array(to_number(df.v1))
+
+#tokenization
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+train_sequences = tokenizer.texts_to_sequences(train_texts)
+
+
+# input matrix
+vocab_size =  len(tokenizer.word_index) +1
+
+# setting matrix dimension
+embedding_dim = 2
+
+# setting max size for embeddings
+max_length = max(map(len, train_sequences)) # using map to become faster
+
+#padding
+tokenized_texts = pad_sequences(train_sequences, maxlen=max_length, padding='post')
+
+# neural network
+model = Sequential()
+model.add(Embedding(input_dim = vocab_size, output_dim = embedding_dim, input_length = max_length)) #embedding layer
+model.add(Flatten()) #making one dimesional 
+model.add(Dense(4, use_bias=True, activation='relu'))
+model.add(Dense(1, use_bias=True, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer="sgd")
+
+model.fit(x = tokenized_texts, y = train_labels, epochs = 100, verbose = 2, validation_freq = 3)
+
+# input array for prediction
+#input_array = ["This is an offer "]
+
+# prediction
+#output_array = model.predict(input_array)
+#print(output_array.shape)
+
+plot_vectors(model, tokenizer)
+
