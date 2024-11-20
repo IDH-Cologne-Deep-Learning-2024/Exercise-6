f8f98611a8f96db6ca0b1197a7e92dd3ff45d8c6
FadeAlex <1fadealex1@gmail.com>
2024-11-20 16:52:30 +0100

    Done

--- embeddings.py
+++ embeddings.py
@@ -5,7 +5,9 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Embedding, Flatten, Input, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+#from sklearn.model_selection import train_test_split
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn import svm 
 
 def to_number(labels):
     number_labels = []
@@ -36,3 +38,46 @@ def plot_vectors(model, tokenizer, indices=[0, 1, 2, 3, 4]):
 df = pd.read_csv("spam.csv", encoding="latin-1")
 train_texts = df.v2.tolist()
 train_labels = np.array(to_number(df.v1))
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+sequences = tokenizer.texts_to_sequences(train_texts)
+max_length = max(len(sequence) for sequence in sequences)
+sequences_padded = pad_sequences(sequences, maxlen = max_length, padding= "post")
+
+FFNN = Sequential()
+FFNN.add(Embedding(vocab_size, 2, input_length= max_length))
+FFNN.add(Flatten())
+FFNN.add(Dense(7, activation="relu"))
+FFNN.add(Dense(1, activation="sigmoid"))
+FFNN.compile(loss='binary_crossentropy', optimizer="sgd")
+
+FFNN.fit(x = sequences_padded, y = train_labels, epochs = 187, verbose = 2, validation_freq = 3)
+
+scam_test_words = [2834, 226, 2744, 47, 318, 8102] #Rich, Money, Gang, Free, Car, Grandma
+plot_vectors(FFNN, tokenizer, scam_test_words)
+
+FFNN.summary()
+
+#Money and grandma are closer than money and rich lul
+
+
+
+
+
+
+"""
+z = df['v2']
+y = df["v1"]
+z_train, z_test,y_train, y_test = train_test_split(z,y,test_size = 0.2)
+
+cv = CountVectorizer()
+features = cv.fit_transform(z_train)
+
+model = svm.SVC()
+model.fit(features,y_train)
+
+features_test = cv.transform(z_test)
+print("Accuracy: {}".format(model.score(features_test,y_test)))
+"""
