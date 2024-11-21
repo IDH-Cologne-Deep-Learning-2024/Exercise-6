50e95de90073d0ecb2f4d5110abc3c846700e4ec
javi1337 <jan@pulsarstudio.de>
2024-11-21 02:54:30 +0100

    My first solution for exercise 6

--- embeddings.py
+++ embeddings.py
@@ -10,9 +10,9 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 def to_number(labels):
     number_labels = []
     for label in labels:
-        if label == 'ham':
+        if label == "ham":
             number_labels.append(0)
-        elif label == 'spam':
+        elif label == "spam":
             number_labels.append(1)
     return number_labels
 
@@ -28,7 +28,15 @@ def plot_vectors(model, tokenizer, indices=[0, 1, 2, 3, 4]):
     vocab = ["[PAD]"] + list(tokenizer.word_index.keys())
     vocab = map(vocab.__getitem__, indices)
     for vector, label in zip(embeddings, vocab):
-        plt.quiver(np.array([0, 0]), np.array([0, 0]), vector[0], vector[1], angles='xy', scale_units='xy', scale=1)
+        plt.quiver(
+            np.array([0, 0]),
+            np.array([0, 0]),
+            vector[0],
+            vector[1],
+            angles="xy",
+            scale_units="xy",
+            scale=1,
+        )
         plt.text(vector[0], vector[1], label)
     plt.show()
 
@@ -36,3 +44,32 @@ def plot_vectors(model, tokenizer, indices=[0, 1, 2, 3, 4]):
 df = pd.read_csv("spam.csv", encoding="latin-1")
 train_texts = df.v2.tolist()
 train_labels = np.array(to_number(df.v1))
+
+# preprocessing the texts
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+tok_texts = tokenizer.texts_to_sequences(train_texts)
+vocab_length = len(tokenizer.word_index) + 1
+max_length = max(len(seq) for seq in tok_texts)
+pad_texts = pad_sequences(tok_texts, maxlen=max_length, padding="post")
+
+
+# feed-forward neural network
+FFNN = Sequential(
+    [
+        Embedding(input_dim=vocab_length, output_dim=2),
+        Flatten(),
+        Dense(15, activation="relu"),
+        Dense(8, activation="relu"),
+        Dense(1, activation="sigmoid"),
+    ]
+)
+
+FFNN.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+FFNN.fit(pad_texts, train_labels, batch_size=32, epochs=25, verbose=1)
+
+# plotting vectors with selected words
+# print(tokenizer.word_index)
+# sel_words are "pls", "sorry", "tomorrow", "ready", "hope" "alright", "you","any"
+sel_words = [104, 83, 151, 315, 126, 518, 3, 105]
+plot_vectors(FFNN, tokenizer, sel_words)
